2bcbf9989a07c3833a382d3c407df1d793b2c40b
valdn <dinardovalentin@gmail.com>
2019-09-20 13:35:28 +0200

    fonction crypto

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 //sudo g++ -DNDEBUG -g2 -O2 -I /usr/local/include/cryptopp/ main.cpp -o test.exe /usr/local/lib/libcryptopp.a
 
 #include "cryptlib.h"
+#include "main.hpp"
 
 #define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
 #include "md5.h"
@@ -11,11 +12,18 @@
 
 int main (int argc, char* argv[])
 {
+	std::string msg;
+	std::string digest;
+	crypto(msg, digest);
+	return 0;
+}
+
+void crypto(std::string msg, std::string digest){
     using namespace CryptoPP;
 	HexEncoder encoder(new FileSink(std::cout));
 
-	std::string msg = "MARC LE BON, LE BEUAU, LE BEUF, LE BEAUF";
-	std::string digest;
+	std::cout << "Entrez votre message" << std::endl;
+	getline(std::cin,msg);
 
 	Weak::MD5 hash;
 	hash.Update((const byte*)msg.data(), msg.size());
@@ -27,6 +35,4 @@ int main (int argc, char* argv[])
 	std::cout << "Digest: ";
 	StringSource(digest, true, new Redirector(encoder));
 	std::cout << std::endl;
-
-	return 0;
 }
---
+++ src/main.hpp
@@ -0,0 +1,3 @@
+
+void crypto(std::string msg, std::string digest);
+
